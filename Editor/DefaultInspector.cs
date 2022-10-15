6c87c2252ebed3e4bab215d68b205d0d5dde9e58
baba-s <shotababa1206@gmail.com>
2022-10-15 09:51:25 +0900

    add

---
+++ Editor/DefaultInspector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    [CanEditMultipleObjects]
+    [CustomEditor( inspectedType: typeof( Object ), editorForChildClasses: true, isFallback = true )]
+    internal sealed class DefaultInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.UpdateIfRequiredOrScript();
+
+            var iterator = serializedObject.GetIterator();
+
+            for ( var enterChildren = true; iterator.NextVisible( enterChildren ); enterChildren = false )
+            {
+                EditorGUILayout.PropertyField( iterator, true );
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.DefaultInspector
+# Kogane Default Inspector
+
+Inspector の Script の欄を有効にするエディタ拡張
